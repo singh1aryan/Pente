7aec4b652b23a799cd85402c5c67e27c2f299fe1
aryan <aspiringaryan@gmail.com>
2018-12-22 22:06:48 -0500

    button clicks

--- clicks.js
+++ clicks.js
@@ -2,11 +2,47 @@ let click = false;
 function buttonclick(){
     var property = document.getElementById('btn01');
     if(!click){
-        property.style.backgroundColor = '#FFFFFF'
+        property.style.backgroundColor = '#FF0000'
         click = true;
     }
     else{
-        property.style.backgroundColor = '#7FFF00'
+        property.style.backgroundColor = '#000000'
         click = false;
     }
 }
+function buttonclick(){
+    var property = document.getElementById('btn01');
+    if(!click){
+        property.style.backgroundColor = '#FF0000'
+        click = true;
+    }
+    else{
+        property.style.backgroundColor = '#000000'
+        click = false;
+    }
+}
+let board_array = [];
+for(let i=0;i<13;++i){
+    let c= [];
+    for(let j=0;j<13;++j){
+        c.push(document.getElementById('btn0'+j));
+    }
+    board_array.push(c);
+}
+
+
+
+// make 12 arrays of the columns
+// add all the arrays to a new array
+// you get a 2D array of the points
+row_array.push(document.getElementById('btn01'),
+    document.getElementById('btn02'),
+    document.getElementById('btn03')
+    
+    );
+
+column_array.push(document.getElementById('btn01'),
+    document.getElementById('btn02'),
+    document.getElementById('btn03')
+    
+    );
