bcf0f82e1935d69981ccf1047c288d9af5fe2ea4
aryan <aspiringaryan@gmail.com>
2018-12-29 23:35:09 -0500

    click handled

--- clicks.js
+++ clicks.js
@@ -1,18 +1,9 @@
 //Yefim Shneyderman and Aryan Singh 2018
-// class Pente{
-    //initializes the board with all 0's to represent blank spaces
-    // 0 is for blank, 1 is for black, 2 is for white
-    // grey - blank
-    // constructor(){
-    //     this.playerTurn = 1;
-    //     this.hasWinner = false;
-    //     this.winner = 0;
-    //     
-    //     this.initialize();
-    // }
+// class Pente
 
+    let playerTurn = 0;
     let board = [];
-        let boardSize = 13;
+    let boardSize = 13;
         for(let i=0;i<13;++i){
         let c= [];
             for(let j=0;j<13;++j){
@@ -22,18 +13,22 @@
             board.push(c);
     }   
 
+    let click = false;
     function buttonclick(id){
             var property = document.getElementById(id);
-
-            playPiece();
-            // if(!click){
-            //     property.style.backgroundColor = '#FF0000'
-            //     click = true;
-            // }
-            // else{
-            //     property.style.backgroundColor = '#000000'
-            //     click = false;
-            // }
+            // playPiece();
+            // have to pass x,y in playPiece
+            let a = id.split(',');
+            playPiece(a[0],a[1]);
+            
+            if(!click){
+                property.style.backgroundColor = '#FF0000'
+                click = true;
+            }
+            else{
+                property.style.backgroundColor = '#000000'
+                click = false;
+            }
         }
 
     //prints a friendly message at the start of the round
@@ -44,9 +39,9 @@
     //prints to the console such that x:0 and y:0 are on top left
     function printBoard() {
         let print = "";
-        for (let x = 0; x < this.boardSize; ++x) {
-            for (let y = 0; y < this.boardSize; ++y) {
-                print += this.board[y][x] + " ";
+        for (let x = 0; x < boardSize; ++x) {
+            for (let y = 0; y < boardSize; ++y) {
+                print += board[y][x] + " ";
             }
             console.log(print);
             print = "";
@@ -55,16 +50,16 @@
 
     //returns the board array
     function getBoard(){
-        return this.board;
+        return  board;
     }
 
     //returns a string formatted such that x:0 and y:0 are on top left of the string
     function toString(){
         let boardString = "";
         let print = "";
-        for (let x = 0; x < this.boardSize; ++x) {
-            for (let y = 0; y < this.boardSize; ++y) {
-                print += this.board[y][x] + " ";
+        for (let x = 0; x <  boardSize; ++x) {
+            for (let y = 0; y <  boardSize; ++y) {
+                print +=  board[y][x] + " ";
             }
             boardString = boardString + print + "\r\n";
             print = "";
@@ -81,14 +76,14 @@
     //plays a piece at the correct spot and calls updateBoard to update player turn and update board
     //only does so if place is not occupied by any other piece
     function playPiece(x,y) {
-        if (!this.isOccupied(x, y)) {
-            if (this.playerTurn === 1) {
-                this.setWhite(x, y);
+        if (!isOccupied(x, y)) {
+            if (playerTurn === 1) {
+                 setWhite(x, y);
             }
             else {
-                this.setBlack(x, y);
+                setBlack(x, y);
             }
-            this.updateBoard();
+            updateBoard();
         }
         else{
             console.log("That square is already occupied.");
@@ -98,29 +93,29 @@
     //returns true if (x,y) is occupied by player 1 or 2
     //returns false if the spot is empty and a new piece can be played there
     function isOccupied(x,y){
-        return(this.getColor(x,y) === 1 || this.getColor(x,y) === 2);
+        return(getColor(x,y) === 1 || getColor(x,y) === 2);
     }
 
     //sets the specified piece to black
     function setBlack (x,y) {
-        this.board[x][y].style.backgroundColor = '#000000';
+        board[x][y].style.backgroundColor = '#000000';
     }
 
     //sets the specified piece to white
     function setWhite (x,y) {
-        this.board[x][y].style.backgroundColor = '#ffffff';
+        board[x][y].style.backgroundColor = '#ffffff';
     }
 
     function setBlank(x,y){
-        this.board[x][y].style.backgroundColor = '#D3D3D3';
+        board[x][y].style.backgroundColor = '#D3D3D3';
     }
 
     //returns the color of the specified piece
     function getColor(x,y) {
-        if(this.board[x][y].style.backgroundColor == '#D3D3D3'){//grey
+        if(board[x][y].style.backgroundColor == '#D3D3D3'){//grey
             return 0;
         }
-        else if(this.board[x][y].style.backgroundColor == '#000000'){//black
+        else if(board[x][y].style.backgroundColor == '#000000'){//black
             return 1;
         }
         else{//white
@@ -130,88 +125,88 @@
 
     //returns true if the two specified pieces are opposite player colors and neither is blank
     function oppositeColors(x1,y1,x2,y2){
-        if(this.getColor(x1,y1) !== 0 && this.getColor(x2,y2) !== 0 && 
-        this.getColor(x1,y1) !== this.getColor(x2,y2)){
+        if(getColor(x1,y1) !== 0 && getColor(x2,y2) !== 0 && 
+        getColor(x1,y1) !== getColor(x2,y2)){
             return true;
         }
         return false;
     }
 
     //resets the game board to all blank
-    function resetBoard(){
-        this.constructor();
-    }
+    // function resetBoard(){
+    //     constructor();
+    // }
 
     //method must be called after every move
     //checks for winners by checking for 5 in a row
     //checks for sandwiches and removes the centers
     //updates the player turn information (however it is designed)
     function updateBoard(){
-        for(let x = 0; x<this.boardSize; ++x) {
-            if(this.hasWinner){break;}
-            for (let y = 0; y < this.boardSize; ++y) {
-                if(this.hasWinner){break;}
-                if(this.getColor(x,y) !== 0){
-                    this.removeSandwich(x,y);
-                    if (this.hasFiveInARow(x,y)) {
-                        this.hasWinner = true;
-                        this.declareWinner(x,y);
+        for(let x = 0; x<boardSize; ++x) {
+            if(hasWinner){break;}
+            for (let y = 0; y < boardSize; ++y) {
+                if(hasWinner){break;}
+                if(getColor(x,y) !== 0){
+                    removeSandwich(x,y);
+                    if (hasFiveInARow(x,y)) {
+                        hasWinner = true;
+                        declareWinner(x,y);
                     }
                 }
             }
         }
-        this.updateTurn();
+         updateTurn();
     }
 
     function updateTurn(){
-        if(this.playerTurn === 1){
-            this.playerTurn = 2;
+        if( playerTurn === 1){
+             playerTurn = 2;
         }
         else{
-            this.playerTurn = 1;
+             playerTurn = 1;
         }
     }
 
     //declares a winner and ends the game
     function declareWinner(x,y){
-        console.log(this.getColor(x,y) + " wins the game!");
-        this.winner = this.getColor(x,y);
+        console.log( getColor(x,y) + " wins the game!");
+         winner =  getColor(x,y);
     }
 
     //detects if there are any sandwiches branching off the current piece and then deletes the centers
     function removeSandwich(x,y){
-        if(this.hasSandwichNorth(x,y)){
-            this.delete(x,y-1);
-            this.delete(x,y-2);
+        if( hasSandwichNorth(x,y)){
+             delete(x,y-1);
+             delete(x,y-2);
         }
-        if(this.hasSandwichNorthEast(x,y)){
-            this.delete(x+1,y-1);
-            this.delete(x+2,y-2);
+        if( hasSandwichNorthEast(x,y)){
+             delete(x+1,y-1);
+             delete(x+2,y-2);
         }
-        if(this.hasSandwichEast(x,y)){
-            this.delete(x+1,y);
-            this.delete(x+2,y);
+        if( hasSandwichEast(x,y)){
+             delete(x+1,y);
+             delete(x+2,y);
         }
-        if(this.hasSandwichSouthEast(x,y)){
-            this.delete(x+1,y+1);
-            this.delete(x+2,y+2);
+        if( hasSandwichSouthEast(x,y)){
+             delete(x+1,y+1);
+             delete(x+2,y+2);
         }
-        if(this.hasSandwichSouth(x,y)){
-            this.delete(x,y+1);
-            this.delete(x,y+2);
+        if( hasSandwichSouth(x,y)){
+             delete(x,y+1);
+             delete(x,y+2);
         }
-        if(this.hasSandwichSouthWest(x,y)){
-            this.delete(x-1,y+1);
-            this.delete(x-1,y+2);
+        if( hasSandwichSouthWest(x,y)){
+             delete(x-1,y+1);
+             delete(x-1,y+2);
         }
-        if(this.hasSandwichWest(x,y)){
-            this.delete(x-1,y);
-            this.delete(x-2,y);
+        if( hasSandwichWest(x,y)){
+             delete(x-1,y);
+             delete(x-2,y);
         }
-        if(this.hasSandwichNorthWest(x,y)){
+        if( hasSandwichNorthWest(x,y)){
             console.log("Removing sandwich");
-            this.delete(x-1,y-1);
-            this.delete(x-2,y-2);
+             delete(x-1,y-1);
+             delete(x-2,y-2);
         }
     }
 
@@ -220,7 +215,7 @@
         if(y-3 < 0){//checks if all nodes exist
             return false;
         }
-        return this.getColor(x, y - 3) === this.getColor(x, y) && this.getColor(x, y - 2) === this.getColor(x, y - 1) && this.oppositeColors(x, y, x, y - 1);
+        return  getColor(x, y - 3) ===  getColor(x, y) &&  getColor(x, y - 2) ===  getColor(x, y - 1) &&  oppositeColors(x, y, x, y - 1);
         //base case
     }
 
@@ -228,39 +223,39 @@
         if(y-3 < 0 || x+3 > 18){//checks if all nodes exist
             return false;
         }
-        return this.getColor(x + 3, y - 3) === this.getColor(x, y) && this.getColor(x + 2, y - 2) === this.getColor(x + 1, y - 1) && this.oppositeColors(x, y, x + 1, y - 1);
+        return  getColor(x + 3, y - 3) ===  getColor(x, y) &&  getColor(x + 2, y - 2) ===  getColor(x + 1, y - 1) &&  oppositeColors(x, y, x + 1, y - 1);
         //base case
     }
 
     function hasSandwichEast(x,y){
-        if(x+3 > this.boardSize-1){//checks if all nodes exist
+        if(x+3 >  boardSize-1){//checks if all nodes exist
             return false;
         }
-        return this.getColor(x + 3, y) === this.getColor(x, y) && this.getColor(x + 2, y) === this.getColor(x + 1, y) && this.oppositeColors(x, y, x + 1, y);
+        return  getColor(x + 3, y) ===  getColor(x, y) &&  getColor(x + 2, y) ===  getColor(x + 1, y) &&  oppositeColors(x, y, x + 1, y);
         //base case
     }
 
     function hasSandwichSouthEast(x,y){
-        if(x+3 > this.boardSize-1 || y+3 > this.boardSize-1){//checks if all nodes exist
+        if(x+3 >  boardSize-1 || y+3 >  boardSize-1){//checks if all nodes exist
             return false;
         }
-        return this.getColor(x + 3, y + 3) === this.getColor(x, y) && this.getColor(x + 2, y + 2) === this.getColor(x + 1, y + 1) && this.oppositeColors(x, y, x + 1, y + 1);
+        return  getColor(x + 3, y + 3) ===  getColor(x, y) &&  getColor(x + 2, y + 2) ===  getColor(x + 1, y + 1) &&  oppositeColors(x, y, x + 1, y + 1);
         //base case
     }
 
     function hasSandwichSouth(x,y){
-        if(y+3 > this.boardSize-1){//checks if all nodes exist
+        if(y+3 >  boardSize-1){//checks if all nodes exist
             return false;
         }
-        return this.getColor(x, y + 3) === this.getColor(x, y) && this.getColor(x, y + 2) === this.getColor(x, y + 1) && this.oppositeColors(x, y, x, y + 1);
+        return  getColor(x, y + 3) ===  getColor(x, y) &&  getColor(x, y + 2) ===  getColor(x, y + 1) &&  oppositeColors(x, y, x, y + 1);
         //base case
     }
 
     function hasSandwichSouthWest(x,y){
-        if(y+3 > this.boardSize-1 || x-3 < 0){//checks if all nodes exist
+        if(y+3 >  boardSize-1 || x-3 < 0){//checks if all nodes exist
             return false;
         }
-        return this.getColor(x - 3, y + 3) === this.getColor(x, y) && this.getColor(x - 2, y + 2) === this.getColor(x - 1, y + 1) && this.oppositeColors(x, y, x - 1, y + 1);
+        return  getColor(x - 3, y + 3) ===  getColor(x, y) &&  getColor(x - 2, y + 2) ===  getColor(x - 1, y + 1) &&  oppositeColors(x, y, x - 1, y + 1);
         //base case
     }
 
@@ -268,7 +263,7 @@
         if(x-3 < 0){//checks if all nodes exist
             return false;
         }
-        return this.getColor(x - 3, y) === this.getColor(x, y) && this.getColor(x - 2, y) === this.getColor(x - 1, y) && this.oppositeColors(x, y, x - 1, y);
+        return getColor(x - 3, y) === getColor(x, y) && getColor(x - 2, y) === getColor(x - 1, y) && oppositeColors(x, y, x - 1, y);
         //base case
     }
 
@@ -276,14 +271,15 @@
         if(x-3 < 0 || y-3 < 0){//checks if all nodes exist
             return false;
         }
-        return this.getColor(x - 3, y - 3) === this.getColor(x, y) && this.getColor(x - 2, y - 2) === this.getColor(x - 1, y - 1) && this.oppositeColors(x, y, x - 1, y - 1);
+        return getColor(x - 3, y - 3) ===  getColor(x, y) &&  getColor(x - 2, y - 2) ===  getColor(x - 1, y - 1) &&  oppositeColors(x, y, x - 1, y - 1);
         //base case
     }
 
 
     //check for 5 in a row in any cardinal direction
     function hasFiveInARow(x,y){
-        return !!(this.north(x, y, 1) || this.northEast(x, y, 1) || this.east(x, y, 1) || this.southEast(x, y, 1) || this.south(x, y, 1) || this.southWest(x, y, 1) || this.west(x, y, 1) || this.northWest(x, y, 1));
+        return !!(north(x, y, 1) || northEast(x, y, 1) || east(x, y, 1) || southEast(x, y, 1) 
+        || south(x, y, 1) || southWest(x, y, 1) || west(x, y, 1) || northWest(x, y, 1));
     }
 
     //checks for 5 in a row at each cardinal direction
@@ -291,8 +287,8 @@
         if(count >= 5){
             return true;
         }
-        else if(y-1 >= 0 && this.getColor(x,y) === this.getColor(x,y-1)){//point must exist
-            return this.north(x, y-1, count+1);
+        else if(y-1 >= 0 && getColor(x,y) === getColor(x,y-1)){//point must exist
+            return north(x, y-1, count+1);
         }
         return(count >= 5);
     }
@@ -301,8 +297,8 @@
         if(count >= 5){
             return true;
         }
-        else if(y-1 >= 0 && x+1 >= 0 && this.getColor(x,y) === this.getColor(x+1,y-1)){//point must exist
-            return this.northEast(x+1, y-1, count+1);
+        else if(y-1 >= 0 && x+1 >= 0 && getColor(x,y) === getColor(x+1,y-1)){//point must exist
+            return northEast(x+1, y-1, count+1);
         }
         return(count >= 5);
     }
@@ -311,8 +307,8 @@
         if(count >= 5){
             return true;
         }
-        else if(x+1 <= this.boardSize-1 && this.getColor(x,y) === this.getColor(x+1,y)){//point must exist
-            return this.east(x+1, y, count+1);
+        else if(x+1 <= boardSize-1 && getColor(x,y) === getColor(x+1,y)){//point must exist
+            return east(x+1, y, count+1);
         }
         return(count >= 5);
     }
@@ -321,8 +317,8 @@
         if(count >= 5){
             return true;
         }
-        else if(y+1 <= this.boardSize-1 && x+1 <= this.boardSize-1 && this.getColor(x,y) === this.getColor(x+1,y+1)){//point must exist
-            return this.southEast(x+1, y+1, count+1);
+        else if(y+1 <= boardSize-1 && x+1 <= boardSize-1 && getColor(x,y) === getColor(x+1,y+1)){//point must exist
+            return southEast(x+1, y+1, count+1);
         }
         return(count >= 5);
     }
@@ -331,8 +327,8 @@
         if(count >= 5){
             return true;
         }
-        else if(y+1 <= this.boardSize-1 && this.getColor(x,y) === this.getColor(x,y+1)){//point must exist
-            return this.south(x, y+1, count+1);
+        else if(y+1 <= boardSize-1 && getColor(x,y) === getColor(x,y+1)){//point must exist
+            return south(x, y+1, count+1);
         }
         return(count >= 5);
     }
@@ -341,8 +337,8 @@
         if(count >= 5){
             return true;
         }
-        else if(y+1 <= this.boardSize-1 && x-1 >= 0 && this.getColor(x,y) === this.getColor(x-1,y+1)){//point must exist
-            return this.southWest(x-1, y+1, count+1);
+        else if(y+1 <= boardSize-1 && x-1 >= 0 && getColor(x,y) === getColor(x-1,y+1)){//point must exist
+            return southWest(x-1, y+1, count+1);
         }
         return(count >= 5);
     }
@@ -351,8 +347,8 @@
         if(count >= 5){
             return true;
         }
-        else if(x-1 >= 0 && this.getColor(x,y) === this.getColor(x-1,y)){//point must exist
-            return this.west(x-1, y, count+1);
+        else if(x-1 >= 0 && getColor(x,y) === getColor(x-1,y)){//point must exist
+            return west(x-1, y, count+1);
         }
         return(count >= 5);
     }
@@ -361,8 +357,8 @@
         if(count >= 5){
             return true;
         }
-        else if(y-1 >= 0 && x-1 >= 0 && this.getColor(x,y) === this.getColor(x-1,y-1)){//point must exist
-            return this.northWest(x-1, y-1, count+1);
+        else if(y-1 >= 0 && x-1 >= 0 && getColor(x,y) === getColor(x-1,y-1)){//point must exist
+            return northWest(x-1, y-1, count+1);
         }
         return(count >= 5);
     }
